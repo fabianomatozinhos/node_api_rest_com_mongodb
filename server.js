57486d496790434ae184e50aea080b75f3f01eb1
fabianomatozinhos <fabianomatozinhos1@hotmail.com>
2023-04-30 20:21:49 -0300

    trabalhando com o Express e criando as rotas do CRUD

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const http = require('http');
-const port = 8080;
+import app from './src/app.js'
 
-const rotas = {
-    '/' : 'Curso de API em Node',
-    '/livros' : 'Entrei na página de livros',
-    '/autores': 'Entrei na págiana de autores',
-    '/editora': 'Entrei na págiana de editoraaaa'
-}
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-});
+// const rotas = {
+//     '/' : 'Curso de API em Node',
+//     '/livros' : 'Entrei na página de livros',
+//     '/autores': 'Entrei na págiana de autores',
+//     '/editora': 'Entrei na págiana de editoraaaa'
+// }
 
-server.listen(port, () => {
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end(rotas[req.url]);
+// });
+
+app.listen(port, () => {
     console.log(`servidor escutando na porta ${port}`)
 })
 
